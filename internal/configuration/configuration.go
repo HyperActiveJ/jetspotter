4eb6f85665878055d2e675951a893108f08da2bb
Peter Jorgensen <6298301+HyperActiveJ@users.noreply.github.com>
2025-05-27 07:45:37 -0700

    Update configuration.go

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -18,22 +18,21 @@ type Config struct {
 	// LOCATION_LATITUDE 51.17348
 	// LOCATION_LONGITUDE 5.45921
 	Location geodist.Coord
+	
+	Location2 geodist.Coord
 
 	// Maximum range in kilometers from the location that you want aircraft to be spotted.
 	// Note that this is an approximation due to roundings.
 	// MAX_RANGE_KILOMETERS 30
 	MaxRangeKilometers int
-
-	// Maximum range in kilometers from the location that you want to scan for aircraft.
-	// This is used to determine the area to query from the ADS-B API.
-	// If not set, MAX_RANGE_KILOMETERS will be used.
-	// MAX_SCAN_RANGE_KILOMETERS 30
-	MaxScanRangeKilometers int
+	MaxRangeKilometers2 int
+	MaxRangeKilometersR int
 
 	// Maximum altitude in feet that you want to spot aircraft at.
 	// Set to 0 to disable the filter.
 	// MAX_ALTITUDE_FEET 0
 	MaxAltitudeFeet int
+	MaxAltitudeFeet2 int
 
 	// A comma seperated list of types that you want to spot
 	// If not set, 'ALL' will be used, which will disable the filter and show all aircraft within range.
@@ -44,6 +43,42 @@ type Config struct {
 	// To spot all military aircraft, you can use MILITARY.
 	// AIRCRAFT_TYPES MILITARY
 	AircraftTypes []string
+
+	// A comma seperated list of types that you want to spot
+	// If not set, 'ALL' will be used, which will disable the filter and show all aircraft within range.
+	// Full list can be found at https://www.icao.int/publications/doc8643/pages/search.aspx in 'Type Designator' column.
+	// AIRCRAFT_TYPES ALL
+	// EXAMPLES
+	// AIRCRAFT_TYPES F16,F35
+	// To spot all military aircraft, you can use MILITARY.
+	// AIRCRAFT_TYPES MILITARY
+	AircraftTypesExcl []string
+	AircraftTypesExcl2 []string
+	
+	AircraftTypesKnownInteresting []string
+	
+	PrintIgnores int
+	RunOnce int
+	PrintApproach int
+	PrintDeparture int
+	PrintInteresting int
+	PrintMil int
+	PrintFlag2 int
+	PritnStrange int
+	PritnImage int
+	PrintStartup int
+	
+	ADSBX int
+	ALIVE int
+	
+
+	RunwayHeading int
+	RunwayApproachAngle int
+
+	// Maximum amount of aircraft to show in a single slack message.
+	// Note that a single slack message only supports up to 50 'blocks' and each aircraft that we display has multiple blocks.
+	// MAX_AIRCRAFT_SLACK_MESSAGE 8
+	MaxAircraftSlackMessage int
 
 	// Webhook used to send notifications to Slack. If not set, no messages will be sent to Slack.
 	// SLACK_WEBHOOK_URL ""
@@ -73,18 +108,6 @@ type Config struct {
 	// METRICS_PORT "7070"
 	MetricsPort string
 
-	// Port where API will be exposed on
-	// API_PORT "8085"
-	APIPort string
-
-	// Enable or disable the web UI
-	// WEB_UI_ENABLED "true"
-	WebUIEnabled bool
-
-	// Port where web UI will be exposed on
-	// WEB_UI_PORT "8080"
-	WebUIPort string
-
 	// Topic to publish message to
 	// NTFY_TOPIC ""
 	NtfyTopic string
@@ -96,24 +119,53 @@ type Config struct {
 
 // Environment variable names
 const (
-	SlackWebhookURL        = "SLACK_WEBHOOK_URL"
-	DiscordWebhookURL      = "DISCORD_WEBHOOK_URL"
-	DiscordColorAltitude   = "DISCORD_COLOR_ALTITUDE"
-	LocationLatitude       = "LOCATION_LATITUDE"
-	LocationLongitude      = "LOCATION_LONGITUDE"
-	MaxRangeKilometers     = "MAX_RANGE_KILOMETERS"
-	MaxScanRangeKilometers = "MAX_SCAN_RANGE_KILOMETERS"
-	MaxAltitudeFeet        = "MAX_ALTITUDE_FEET"
-	AircraftTypes          = "AIRCRAFT_TYPES"
-	FetchInterval          = "FETCH_INTERVAL"
-	GotifyURL              = "GOTIFY_URL"
-	NtfyTopic              = "NTFY_TOPIC"
-	NtfyServer             = "NTFY_SERVER"
-	GotifyToken            = "GOTIFY_TOKEN"
-	MetricsPort            = "METRICS_PORT"
-	APIPort                = "API_PORT"
-	WebUIEnabled           = "WEB_UI_ENABLED"
-	WebUIPort              = "WEB_UI_PORT"
+	SlackWebhookURL          = "SLACK_WEBHOOK_URL"
+	DiscordWebhookURL        = "DISCORD_WEBHOOK_URL"
+	DiscordColorAltitude     = "DISCORD_COLOR_ALTITUDE"
+	LocationLatitude         = "LOCATION_LATITUDE"
+	LocationLongitude        = "LOCATION_LONGITUDE"
+	MaxRangeKilometers       = "MAX_RANGE_KILOMETERS"
+	MaxAltitudeFeet          = "MAX_ALTITUDE_FEET"
+	MaxAircrfaftSlackMessage = "MAX_AIRCRAFT_SLACK_MESSAGE"
+	AircraftTypes            = "AIRCRAFT_TYPES"
+	AircraftTypesExcl        = "AIRCRAFT_TYPES_EXCL"
+	FetchInterval            = "FETCH_INTERVAL"
+	GotifyURL                = "GOTIFY_URL"
+	NtfyTopic                = "NTFY_TOPIC"
+	NtfyServer               = "NTFY_SERVER"
+	GotifyToken              = "GOTIFY_TOKEN"
+	MetricsPort              = "METRICS_PORT"
+	
+	AircraftTypesKnownInteresting = "AIRCRAFT_TYPES_CONFIRMED"
+	
+	PrintIgnores = "PRINT_IGNORED"
+	
+	ADSBX = "ADSBX"
+	ALIVE = "ALIVE"
+	
+	RunOnce = "RUN_ONCE"
+	PrintApproach  = "PRINT_APPROACH"
+	PrintDeparture  = "PRINT_DEPARTURE"
+	PrintInteresting  = "PRINT_INTERESTING"
+	PrintMil  = "PRINT_MIL"
+	PrintFlag2  = "PRINT_FLAG2"
+	PritnStrange  = "PRINT_STRANGE"
+	PritnImage  = "PRINT_IMAGE"
+	PrintStartup = "PRINT_STARTUP"
+
+	
+	RunwayHeading 			= "RUNWAY_HEADING"
+	RunwayApproachAngle 	= "APPROACH_ANGLE_WINDOW"
+	
+	MaxRangeKilometers2       = "MAX_RANGE_KILOMETERS2"
+	MaxAltitudeFeet2          = "MAX_ALTITUDE_FEET2"
+	AircraftTypesExcl2        = "AIRCRAFT_TYPES_EXCL2"
+	
+	MaxRangeKilometersR       = "MAX_RANGE_KILOMETERS_REQUEST"
+	
+	LocationLatitude2         = "LOCATION_LATITUDE2"
+	LocationLongitude2        = "LOCATION_LONGITUDE2"
+	
 )
 
 // getEnvVariable looks up a specified environment variable, if not set the specified default is used
@@ -137,19 +189,8 @@ func GetConfig() (config Config, err error) {
 	config.DiscordWebHookURL = getEnvVariable(DiscordWebhookURL, "")
 	config.DiscordColorAltitude = getEnvVariable(DiscordColorAltitude, "true")
 	config.MetricsPort = getEnvVariable(MetricsPort, "7070")
-	config.APIPort = getEnvVariable(APIPort, "8085")
-	config.WebUIPort = getEnvVariable(WebUIPort, "8080")
-
-	// Convert WebUIEnabled string to bool
-	webUIEnabledStr := getEnvVariable(WebUIEnabled, "true")
-	config.WebUIEnabled, err = strconv.ParseBool(webUIEnabledStr)
-	if err != nil {
-		log.Printf("Invalid value for WEB_UI_ENABLED: %s, using default: true", webUIEnabledStr)
-		config.WebUIEnabled = true
-	}
-
 	config.FetchInterval, err = strconv.Atoi(getEnvVariable(FetchInterval, strconv.Itoa(defaultFetchInterval)))
-	if err != nil || config.FetchInterval < 5 {
+	if err != nil || config.FetchInterval < 60 {
 		log.Printf("Fetch interval of %ds detected. You might hit rate limits, consider using the default of %ds instead.", config.FetchInterval, defaultFetchInterval)
 	}
 
@@ -162,21 +203,20 @@ func GetConfig() (config Config, err error) {
 	if err != nil {
 		return Config{}, err
 	}
-
-	config.MaxRangeKilometers, err = strconv.Atoi(getEnvVariable(MaxRangeKilometers, "30"))
+	
+	config.Location2.Lat, err = strconv.ParseFloat(getEnvVariable(LocationLatitude2, "51.17348"), 64)
 	if err != nil {
 		return Config{}, err
 	}
 
-	// Get MAX_SCAN_RANGE_KILOMETERS - If not set, use MAX_RANGE_KILOMETERS for backward compatibility
-	maxScanRangeStr := getEnvVariable(MaxScanRangeKilometers, "")
-	if maxScanRangeStr == "" {
-		config.MaxScanRangeKilometers = config.MaxRangeKilometers
-	} else {
-		config.MaxScanRangeKilometers, err = strconv.Atoi(maxScanRangeStr)
-		if err != nil {
-			return Config{}, err
-		}
+	config.Location2.Lon, err = strconv.ParseFloat(getEnvVariable(LocationLongitude2, "5.45921"), 64)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.MaxRangeKilometers, err = strconv.Atoi(getEnvVariable(MaxRangeKilometers, "30"))
+	if err != nil {
+		return Config{}, err
 	}
 
 	config.MaxAltitudeFeet, err = strconv.Atoi(getEnvVariable(MaxAltitudeFeet, "0"))
@@ -184,6 +224,54 @@ func GetConfig() (config Config, err error) {
 		return Config{}, err
 	}
 
+	config.MaxAircraftSlackMessage, err = strconv.Atoi(getEnvVariable(MaxAircrfaftSlackMessage, "8"))
+	if err != nil {
+		return Config{}, err
+	}
+
 	config.AircraftTypes = strings.Split(strings.ToUpper(strings.ReplaceAll(getEnvVariable(AircraftTypes, "ALL"), " ", "")), ",")
+	
+	config.AircraftTypesExcl = strings.Split(strings.ToUpper(strings.ReplaceAll(getEnvVariable(AircraftTypesExcl, ""), " ", "")), ",")
+	
+	config.AircraftTypesKnownInteresting = strings.Split(strings.ToUpper(strings.ReplaceAll(getEnvVariable(AircraftTypesKnownInteresting, ""), " ", "")), ",")
+
+	
+	config.RunwayHeading, err  = strconv.Atoi(getEnvVariable(RunwayHeading, "89"))
+	config.RunwayApproachAngle, err  = strconv.Atoi(getEnvVariable(RunwayApproachAngle, "20"))
+	
+	config.PrintIgnores, err  = strconv.Atoi(getEnvVariable(PrintIgnores, "0"))
+	config.RunOnce, err  = strconv.Atoi(getEnvVariable(RunOnce, "0"))
+	
+	config.PrintApproach, err  = strconv.Atoi(getEnvVariable(PrintApproach, "0"))
+	config.PrintDeparture, err  = strconv.Atoi(getEnvVariable(PrintDeparture, "0"))
+	config.PrintInteresting, err  = strconv.Atoi(getEnvVariable(PrintInteresting, "0"))
+	config.PrintMil, err  = strconv.Atoi(getEnvVariable(PrintMil, "0"))
+	config.PrintFlag2, err  = strconv.Atoi(getEnvVariable(PrintFlag2, "0"))
+	config.PritnStrange, err  = strconv.Atoi(getEnvVariable(PritnStrange, "0"))
+	config.PritnImage, err  = strconv.Atoi(getEnvVariable(PritnImage, "0"))
+	config.PrintStartup, err  = strconv.Atoi(getEnvVariable(PrintStartup, "1"))
+	
+	config.ADSBX, err  = strconv.Atoi(getEnvVariable(ADSBX, "0"))
+	config.ALIVE, err  = strconv.Atoi(getEnvVariable(ALIVE, "1"))
+
+	config.MaxRangeKilometersR, err = strconv.Atoi(getEnvVariable(MaxRangeKilometersR, "30"))
+	if err != nil {
+		return Config{}, err
+	}
+
+	
+	config.MaxRangeKilometers2, err = strconv.Atoi(getEnvVariable(MaxRangeKilometers2, "30"))
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.MaxAltitudeFeet2, err = strconv.Atoi(getEnvVariable(MaxAltitudeFeet2, "0"))
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.AircraftTypesExcl2 = strings.Split(strings.ToUpper(strings.ReplaceAll(getEnvVariable(AircraftTypesExcl2, ""), " ", "")), ",")
+	
 	return config, nil
+	
 }
